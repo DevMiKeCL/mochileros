d72be6eead25a99530dcc9c56004cc042ac54a17
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-05 20:24:03 -0300

    titulo

--- addplace.php
+++ addplace.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <title>Formulario</title>
+    <title>Crear Lugar</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
