f6df4813247fe511b5a2d75fe98c3583ace13ba1
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-08 20:09:34 -0300

    BBDD Mochileros

--- mochileros.sql
+++ mochileros.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     03-12-2016 4:41:35                           */
+/* Created on:     08-12-2016 20:08:07                          */
 /*==============================================================*/
 
 
@@ -8,11 +8,19 @@ drop table if exists ADMINISTRADOR;
 
 drop table if exists BUSQUEDA;
 
-drop table if exists CLIENTE;
+drop table if exists CONTADO;
+
+drop table if exists CONTRATO;
+
+drop table if exists CREDITO;
 
 drop table if exists LUGAR;
 
 drop table if exists TIPO_LUGAR;
+
+drop table if exists TIPO_USUARIO;
+
+drop table if exists UBICACION_ACTUAL;
 
 drop table if exists USUARIO;
 
@@ -27,7 +35,6 @@ create table ADMINISTRADOR
    A_APATERNO           varchar(20),
    A_AMATERNO           varchar(20),
    A_EMAIL              varchar(50),
-   A_GMAP               varchar(50),
    primary key (A_RUT)
 );
 
@@ -46,23 +53,41 @@ create table BUSQUEDA
 );
 
 /*==============================================================*/
-/* Table: CLIENTE                                               */
+/* Table: CONTADO                                               */
 /*==============================================================*/
-create table CLIENTE
+create table CONTADO
 (
-   C_RUT                varchar(10) not null,
-   C_PASS               varchar(20),
-   C_NOMBRE             varchar(20),
-   C_APATERNO           varchar(20),
-   C_AMATERNO           varchar(20),
-   C_DIRECCION          varchar(20),
-   C_CIUDAD             varchar(20),
-   C_COMUNA             varchar(20),
-   C_TELEFONO           int,
-   C_FNAC               date,
-   C_GMAP               varchar(50),
-   C_EMAIL              varchar(50),
-   primary key (C_RUT)
+   ID_CONTADO           int not null auto_increment,
+   ID_CONTRATO          int,
+   CON_ESTADO           bool,
+   primary key (ID_CONTADO)
+);
+
+/*==============================================================*/
+/* Table: CONTRATO                                              */
+/*==============================================================*/
+create table CONTRATO
+(
+   ID_CONTRATO          int not null auto_increment,
+   FEC_INICIO           date,
+   FEC_TERMINO          date,
+   MODO_PAGO            varchar(20),
+   VALOR                int,
+   primary key (ID_CONTRATO)
+);
+
+/*==============================================================*/
+/* Table: CREDITO                                               */
+/*==============================================================*/
+create table CREDITO
+(
+   ID_CREDITO           int not null auto_increment,
+   ID_CONTRATO          int,
+   C_MES                int,
+   C_FPAGO              date,
+   C_MONTO              int,
+   C_ESTADO             bool,
+   primary key (ID_CREDITO)
 );
 
 /*==============================================================*/
@@ -70,10 +95,10 @@ create table CLIENTE
 /*==============================================================*/
 create table LUGAR
 (
-   ID_LUGAR             int not null auto_increment,
+   ID_LUGAR             int not null,
    A_RUT                varchar(10),
    ID_TIPO              int,
-   C_RUT                varchar(10),
+   U_RUT                varchar(10),
    L_NOMBRE             varchar(20),
    L_DIRECCION          varchar(20),
    L_CIUDAD             varchar(20),
@@ -103,21 +128,48 @@ create table TIPO_LUGAR
 );
 
 /*==============================================================*/
+/* Table: TIPO_USUARIO                                          */
+/*==============================================================*/
+create table TIPO_USUARIO
+(
+   ID_TUSUARIO          int not null,
+   T_NOMBRE             varchar(20),
+   primary key (ID_TUSUARIO)
+);
+
+/*==============================================================*/
+/* Table: UBICACION_ACTUAL                                      */
+/*==============================================================*/
+create table UBICACION_ACTUAL
+(
+   ID_UBICACION         int not null auto_increment,
+   U_RUT                varchar(10),
+   UB_UACTUAL           longtext,
+   UB_FECHA             datetime,
+   primary key (ID_UBICACION)
+);
+
+/*==============================================================*/
 /* Table: USUARIO                                               */
 /*==============================================================*/
 create table USUARIO
 (
    U_RUT                varchar(10) not null,
+   ID_CONTRATO          int,
+   ID_TUSUARIO          int,
    U_PASS               varchar(20),
    U_NOMBRE             varchar(20),
    U_APATERNO           varchar(20),
    U_AMATERNO           varchar(20),
+   U_DIRECCION          varchar(20),
    U_CIUDAD             varchar(20),
    U_COMUNA             varchar(20),
    U_TELEFONO           int,
    U_FNAC               date,
-   U_GMAP               varchar(50),
    U_EMAIL              varchar(50),
+   U_CACTIVACION        varchar(20),
+   U_ESTADO             bool,
+   U_PAIS               varchar(50),
    primary key (U_RUT)
 );
 
@@ -127,12 +179,27 @@ alter table BUSQUEDA add constraint FK_BUSCA foreign key (U_RUT)
 alter table BUSQUEDA add constraint FK_BUSCA2 foreign key (ID_LUGAR)
       references LUGAR (ID_LUGAR) on delete restrict on update restrict;
 
-alter table LUGAR add constraint FK_CREA foreign key (C_RUT)
-      references CLIENTE (C_RUT) on delete restrict on update restrict;
+alter table CONTADO add constraint FK_CONTIENE2 foreign key (ID_CONTRATO)
+      references CONTRATO (ID_CONTRATO) on delete restrict on update restrict;
+
+alter table CREDITO add constraint FK_CONTIENE foreign key (ID_CONTRATO)
+      references CONTRATO (ID_CONTRATO) on delete restrict on update restrict;
+
+alter table LUGAR add constraint FK_CREA foreign key (U_RUT)
+      references USUARIO (U_RUT) on delete restrict on update restrict;
 
 alter table LUGAR add constraint FK_PUBLICA foreign key (A_RUT)
       references ADMINISTRADOR (A_RUT) on delete restrict on update restrict;
 
 alter table LUGAR add constraint FK_TIENE foreign key (ID_TIPO)
       references TIPO_LUGAR (ID_TIPO) on delete restrict on update restrict;
+
+alter table UBICACION_ACTUAL add constraint FK_POSEE foreign key (U_RUT)
+      references USUARIO (U_RUT) on delete restrict on update restrict;
+
+alter table USUARIO add constraint FK_ADQUIERE foreign key (ID_CONTRATO)
+      references CONTRATO (ID_CONTRATO) on delete restrict on update restrict;
+
+alter table USUARIO add constraint FK_PERTENECE foreign key (ID_TUSUARIO)
+      references TIPO_USUARIO (ID_TUSUARIO) on delete restrict on update restrict;
 
