c5defaa4f6dbe0fd30abf47a7433951b3193564d
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-10 00:46:04 -0300

    ubicacion 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,31 @@ te = String.fromCharCode(tecla); // 5
 return patron.test(te); // 6
 }
 
-var x = document.getElementById("ubicacion");
-function myUbicacion() {
-        navigator.geolocation.getCurrentPosition(showPosition);
-}
-function showPosition(position) {
-    x.innerHTML = "Latitud: " + position.coords.latitude +
-                  "<br>Longitud: " + position.coords.longitude;
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
 }
