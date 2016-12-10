63a4725645a6822ca2de19ea8a0d7a9ef7201b2a
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-10 00:18:23 -0300

    script ubicacion

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,12 @@ ppatron = /\d/; // Solo acepta números// 4
 te = String.fromCharCode(tecla); // 5
 return patron.test(te); // 6
 }
+
+var x = document.getElementById("ubicacion");
+function myUbicacion() {
+        navigator.geolocation.getCurrentPosition(showPosition);
+}
+function showPosition(position) {
+    x.innerHTML = "Latitud: " + position.coords.latitude +
+                  "<br>Longitud: " + position.coords.longitude;
+}
