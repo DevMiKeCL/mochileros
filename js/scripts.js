120576848151cc31426fadea3463cfca8e5c02c9
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-13 00:07:39 -0300

    Update crear usuario

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function validar(e) { // 1
 tecla = (document.all) ? e.keyCode : e.which; // 2
 if (tecla==8) return true; // 3
-patron =/[A-Za-z\s]/; // 4
+patron =/[A-Za-zÑñáéíóúÁÉÍÓÚ]/; // 4
 te = String.fromCharCode(tecla); // 5
 return patron.test(te); // 6
 }
