93ff13f5394660a12843baa79f5397f1279e2f21
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-12-24 19:02:38 -0300

    eliminar una linea

--- obtenerip.php
+++ obtenerip.php
@@ -7,7 +7,7 @@
   $fecha1 = '2012-10-24 14:46:00';
   $fecha2 = '2012-10-24 14:00:00';
   $minutos = ceil((strtotime($fecha1) - strtotime($fecha2)) /60 );
-  //echo "tienes $minutos";
+
   if ($minutos > 45) {
        echo 'tienes '.$minutos.' minuto(s) esto registra como una nueva visita';
   }else {
